45ee90f549f64c15480fb5e1ed7380752abf71e1
dimastadeoo <dimastadeoo@gmail.com>
2026-08-03 22:09:24 +0700

    feat: add Swagger documentation setup and profile routes integration

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,36 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes.js';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+import profileRouter from './profileRoutes.js'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Notes App Backend',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        token: {
+          type: "apiKey",
+          name: "Authorization",
+          in: "header"
+        }
+      }
+    },
+  },
+    
+  apis: ['./src/routes/*.js'],
+};
 
 const router = Router();
+
+const jsdocSetup = swaggerJSDoc(options);
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(jsdocSetup));
+
 router.use('/auth', authRoutes);
+router.use("/profile", profileRouter);
 
 export default router;
