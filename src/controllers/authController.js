d759e02fa77b639d4eb102f82ec2d35bcde356fa
dimastadeoo <dimastadeoo@gmail.com>
2026-08-11 16:08:45 +0700

    refactor: authController to use Sequelize for user and role management

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,14 @@
-import { findByEmail, createUser, findUserWithRole } from '../models/users.models.js';
+// import { findByEmail, createUser, findUserWithRole } from '../models/users.models.js';
 import * as Response from "../lib/response.js";
 import { constants } from "node:http2";
 import libJwt from '../lib/jwt.js';
+import db from '../models/index.cjs'
 import bcrypt from "bcrypt";
-import { getRoleByName } from '../models/roles.models.js';
-import { createProfile } from '../models/profile.models.js';
+// import { getRoleByName } from '../models/roles.models.js';
+// import { createProfile } from '../models/profile.models.js';
+
+const {Users, Profiles, Roles} = db
+
 
 const saltRounds = 10;
 /**
@@ -19,19 +23,34 @@ export async function register(req, res) {
     if (!email || !password || !name) {
       return Response.errorResponse(res, 'Email Or password Or name required', constants.HTTP_STATUS_BAD_REQUEST);
     }
-    const customerRole = await getRoleByName("customer")
+    const customerRole = await Roles.findOne({where: {name:"customer"}})
 
-    const existing = await findByEmail(email);
+    const data = await Users.findAll()
+    const existing = data.find(u => u.email === email)
     if (existing) {
       return Response.errorResponse(res, 'Email already exists', constants.HTTP_STATUS_BAD_REQUEST);
-
     }
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const user = await createUser(email, hashedPassword, customerRole.id);
-    const profile = await createProfile(user.id, name)
-    const results = { name: profile.name, email: user.email };
 
-    Response.successResponse(res, 'User registered successfully', results, constants.HTTP_STATUS_CREATED);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const user = await await Users.create({
+      email: email, password:hashedPassword, id_role:customerRole.id
+    });
+    await Profiles.create({name: name, id_user: user.id})
+
+    const result = await Users.findByPk(user.id, {
+      attributes: { exclude: ['password', 'id', 'id_role'] },
+      include: [{
+        model: Profiles,
+        as: 'profile',
+        attributes: { exclude: ['password', 'id', 'id_user', 'created_at', 'updated_at'] },
+      },{
+        model: Roles,
+        as: 'role',
+        attributes: ['name']
+      }]
+    })
+
+    Response.successResponse(res, 'User registered successfully', result, constants.HTTP_STATUS_CREATED);
 
   } catch (error) {
     const err = "Fail Register Data Because " + error;
@@ -52,14 +71,14 @@ export async function login(req, res) {
       return Response.errorResponse(res, 'Email and password required', constants.HTTP_STATUS_BAD_REQUEST);
     }
 
-    const user = await findUserWithRole(email);
+    const user = await Users.findOne({where: {email: email}});
     const passwordCheck = await bcrypt.compare(password, user.password);
     if (!user || !passwordCheck) {
       return Response.errorResponse(res, 'User or password wrong', constants.HTTP_STATUS_UNAUTHORIZED);
     }
-    
+    const customerRole = await Roles.findByPk(user.id_role)
     const token = libJwt.sign({userId: user.id});
-    const results = { token: token, user: { email: user.email, role: user.role_name } };
+    const results = { token: token, user: { email: user.email, role: customerRole.name } };
     Response.successResponse(res, `User ${user.email} Login successfully`, results);
 
   } catch (error) {
